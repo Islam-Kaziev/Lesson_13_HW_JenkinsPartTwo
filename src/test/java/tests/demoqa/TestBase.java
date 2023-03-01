b534fa5cfbdc792f682af449f37e639e99dbdb6d
Islam-Kaziev <kaziev19691@gmail.com>
2023-03-02 00:09:18 +0300

    fix build.gradle

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -14,12 +14,13 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.holdBrowserOpen = false;
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100";
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.timeout = 10000;  //10 сек
+
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
